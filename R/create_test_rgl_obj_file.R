dcd487f30848c966a4923952367a1b5adb2585de
Joachim Gassen <gassen@wiwi.hu-berlin.de>
2020-12-03 06:29:00 +0100

    move to transportation example

--- R/create_test_rgl_obj_file.R
+++ R/create_test_rgl_obj_file.R
@@ -3,6 +3,10 @@ library(rgl)
 plotids <- with(iris, plot3d(Sepal.Length, Sepal.Width, Petal.Length, 
                              type="s", col=as.numeric(Species)))
 writeOBJ("~/Dropbox/test_rgl.obj")
+writeSTL("~/Dropbox/test_rgl.stl")
+writePLY("~/Dropbox/test_rgl.ply")
+writeOB
+# All files do not contain text...
 
-# Use blender to convert to glb (levy dropbox for that)
+# Use blender to convert to gltf (separate) (levy docker for that?)
 # store in media
